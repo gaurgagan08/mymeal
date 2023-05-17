e7131a023a45a8ff23581e4dcaebb0d9ccf3b736
gaurgagan08 <gagangaur082001@gmail.com>
2023-05-17 15:13:30 +0530

    adding myMeal project

---
+++ script.js
@@ -0,0 +1,47 @@
+window.addEventListener("scroll", function(){
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+$(document).ready(function(){
+    $('.menu-toggle').click(function(){
+        $('.menu-toggle').toggleClass('active')
+        $('nav').toggleClass('active')
+        $('header').toggleClass('active')
+    })
+});
+
+$('#home-button').click(function(){
+    location.href = "food-menu.html";
+});
+
+
+const swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    },
+  });
